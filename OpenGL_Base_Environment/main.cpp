bcd4401c877485b7c3066c7f91c52fc00396723d
曹弘扬 <296786475@qq.com>
2019-01-09 16:51:20 +0800

    init OpenGL Environment

---
+++ OpenGL_Base_Environment/main.cpp
@@ -0,0 +1,217 @@
+/*
+ 课程名称：OpenGL 视觉班--第二次课
+ 案例名称：OpenGL demo1
+ 实现功能：
+    点击屏幕，将固定位置上的顶点数据以6种不同形态展示！
+ */
+
+
+#include "GLTools.h"	
+#include "GLMatrixStack.h"
+#include "GLFrame.h"
+#include "GLFrustum.h"
+#include "GLBatch.h"
+#include "GLGeometryTransform.h"
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+/*
+ GLMatrixStack 变化管线使用矩阵堆栈
+ 
+ GLMatrixStack 构造函数允许指定堆栈的最大深度、默认的堆栈深度为64.这个矩阵堆在初始化时已经在堆栈中包含了单位矩阵。
+ GLMatrixStack::GLMatrixStack(int iStackDepth = 64);
+ 
+ //通过调用顶部载入这个单位矩阵
+ void GLMatrixStack::LoadIndentiy(void);
+ 
+ //在堆栈顶部载入任何矩阵
+ void GLMatrixStack::LoadMatrix(const M3DMatrix44f m);
+ */
+// 各种需要的类
+GLShaderManager		shaderManager;
+GLMatrixStack		modelViewMatrix;
+GLMatrixStack		projectionMatrix;
+GLFrame				cameraFrame;
+GLFrame             objectFrame;
+//投影矩阵
+GLFrustum			viewFrustum;
+
+//容器类（7种不同的图元对应7种容器对象）
+GLBatch				pointBatch;
+GLBatch				lineBatch;
+GLBatch				lineStripBatch;
+GLBatch				lineLoopBatch;
+GLBatch				triangleBatch;
+GLBatch             triangleStripBatch;
+GLBatch             triangleFanBatch;
+
+//几何变换的管道
+GLGeometryTransform	transformPipeline;
+M3DMatrix44f		shadowMatrix;
+
+
+GLfloat vGreen[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+GLfloat vBlack[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+
+// 跟踪效果步骤
+int nStep = 0;
+
+
+
+// 此函数在呈现上下文中进行任何必要的初始化。.
+// 这是第一次做任何与opengl相关的任务。
+void SetupRC()
+{
+    
+    //定义一些点，类似佛罗里达州的形状。
+    GLfloat vCoast[24][3] = {
+        {2.80, 1.20, 0.0 }, {2.0,  1.20, 0.0 },
+        {2.0,  1.08, 0.0 },  {2.0,  1.08, 0.0 },
+        {0.0,  0.80, 0.0 },  {-.32, 0.40, 0.0 },
+        {-.48, 0.2, 0.0 },   {-.40, 0.0, 0.0 },
+        {-.60, -.40, 0.0 },  {-.80, -.80, 0.0 },
+        {-.80, -1.4, 0.0 },  {-.40, -1.60, 0.0 },
+        {0.0, -1.20, 0.0 },  { .2, -.80, 0.0 },
+        {.48, -.40, 0.0 },   {.52, -.20, 0.0 },
+        {.48,  .20, 0.0 },   {.80,  .40, 0.0 },
+        {1.20, .80, 0.0 },   {1.60, .60, 0.0 },
+        {2.0, .60, 0.0 },    {2.2, .80, 0.0 },
+        {2.40, 1.0, 0.0 },   {2.80, 1.0, 0.0 }};
+    
+
+
+}
+
+
+
+void DrawWireFramedBatch(GLBatch* pBatch)
+{
+    
+}
+
+
+
+// 召唤场景
+void RenderScene(void)
+{
+
+}
+
+
+//特殊键位处理（上、下、左、右移动）
+void SpecialKeys(int key, int x, int y)
+{
+
+}
+
+
+
+
+//根据空格次数。切换不同的“窗口名称”
+//key:敲击的键位
+//x,y: 光标的位置
+void KeyPressFunc(unsigned char key, int x, int y)
+{
+    //key == 32 32的ASC2码，是空格。比如A：65; B = 66
+    if(key == 32)
+    {
+        nStep++;
+        
+        if(nStep > 6)
+            nStep = 0;
+    }
+    
+    switch(nStep)
+    {
+        case 0:
+            glutSetWindowTitle("GL_POINTS");
+            break;
+        case 1:
+            glutSetWindowTitle("GL_LINES");
+            break;
+        case 2:
+            glutSetWindowTitle("GL_LINE_STRIP");
+            break;
+        case 3:
+            glutSetWindowTitle("GL_LINE_LOOP");
+            break;
+        case 4:
+            glutSetWindowTitle("GL_TRIANGLES");
+            break;
+        case 5:
+            glutSetWindowTitle("GL_TRIANGLE_STRIP");
+            break;
+        case 6:
+            glutSetWindowTitle("GL_TRIANGLE_FAN");
+            break;
+    }
+    
+    glutPostRedisplay();
+}
+
+//鼠标点击事件
+void MouseKey(int button,int state, int x,int y)
+{
+    
+}
+
+// 窗口已更改大小，或刚刚创建。无论哪种情况，我们都需要
+// 使用窗口维度设置视口和投影矩阵.
+void ChangeSize(int w, int h)
+{
+
+}
+
+
+int main(int argc, char* argv[])
+{
+    gltSetWorkingDirectory(argv[0]);
+    glutInit(&argc, argv);
+    
+    //申请一个颜色缓存区、深度缓存区、双缓存区、模板缓存区
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    
+    //设置window 的尺寸
+    glutInitWindowSize(800, 600);
+    
+    //创建window的名称
+    glutCreateWindow("GL_POINTS");
+    
+    //注册回调函数（改变尺寸）
+    glutReshapeFunc(ChangeSize);
+    
+    //点击空格时，调用的函数
+    //注册一个键盘ASC2 码的键位事件的回调函数，当有键盘按下时，则执行回调函数
+    glutKeyboardFunc(KeyPressFunc);
+    
+    //特殊键位函数（上下左右）
+    glutSpecialFunc(SpecialKeys);
+    
+    //注册鼠标点击事件：
+    glutMouseFunc(MouseKey);
+    
+    
+    //显示函数
+    glutDisplayFunc(RenderScene);
+    
+    //判断一下是否能初始化glew库，确保项目能正常使用OpenGL 框架
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+    
+    //绘制
+    SetupRC();
+    
+    //runloop运行循环
+    glutMainLoop();
+    return 0;
+}
